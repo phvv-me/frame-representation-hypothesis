4d2eca6f4a7b77049940913c2006b27dedf2335c
pvalois <vaz.valois@hotmail.com>
2025-03-30 19:52:34 +0900

    new plots done

--- qsub.sh
+++ qsub.sh
@@ -3,7 +3,7 @@
 #PBS -A CVLABPJ
 #PBS -q gen_S
 #PBS -b 1
-#PBS -l elapstim_req=2:00:00
+#PBS -l elapstim_req=4:00:00
 #PBS –M vaz.valois@hotmail.com
 #PBS –m e #バッチリクエスト終了時に指定したメールアドレスに通知
 #PBS -N sub_pvalois #リクエスト名
@@ -15,6 +15,6 @@ module load openmpi/$NQSV_MPI_VER
 cd $PBS_O_WORKDIR
 
 uv run python -c "import torch; print(torch.__version__)"
-make run
+make run_notebook N=08_guided_generation_model_comparison.ipynb
 
 exit 0
